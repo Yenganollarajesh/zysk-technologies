65b65d652302a679bf9e9e2924114d4523482aa4
Yenganollarajesh <rajeshyenganolla@gmail.com>
2024-11-20 13:25:12 +0530

    added header

---
+++ src/components/Header.js
@@ -0,0 +1,106 @@
+import React, { useState } from 'react';
+import './Header.css';
+
+import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlayCircle } from '@fortawesome/free-solid-svg-icons';
+// import Socialproofsection from "./Socialproofsection"
+// import Featuressection from "./Featuressection"
+// import Testimonialsection from "./Testimonialsection"
+// import Advancedfeatures from "./Advancedfeatures"
+// import FaqSection from "./FaqSection"
+
+
+
+const Header = () => {
+    const [menuOpen, setMenuOpen] = useState(false);   
+
+    const handleMenuToggle = () => {
+        setMenuOpen(!menuOpen);  // Toggle the menu state
+    };
+
+    const profile = require('./images/rajesh photo.jpeg');
+    const right = require('./images/istockphoto-1437816897-612x612.jpg');
+    const stats=require('./images/Hero header section.png')
+    const stats2=require('./images/Screenshot 2024-11-19 233531.png')
+
+    return (
+        <div>
+            <header className="header">
+                <div className="header-left">
+                    {/* Left content including the profile image and message */}
+                    <div className="header-profile">
+                        <img
+                            className="profile-photo"
+                            src={right}
+                            alt="Profile"
+                        />
+                        <p className="message">Untitled Message</p>
+                    </div>
+
+                    {/* Navigation links */}
+                    <nav className={`navigation ${menuOpen ? 'open' : ''}`}>
+                        <ul>
+                            <li><a href="#home"  >Home</a></li>
+                            <li><a href="#products"  >Products</a></li>
+                            <li><a href="#resources"  >Resources</a></li>
+                            <li><a href="#pricing"  >Pricing</a></li>
+                        </ul>
+                    </nav>
+                </div>
+
+                {/* Menu Icon (visible only on small screens) */}
+                <div className="menu-icon" onClick={handleMenuToggle}>
+                    <span className="hamburger-icon">☰</span>
+                </div>
+
+                {/* Right profile image (only for large screens) */}
+                <div className="header-right">
+                    <img
+                        className="profile-photo"
+                        src={profile}
+                        alt="Profile"
+                    />
+                </div>
+            </header>
+
+            <div className="content">
+                <div className="content-box">
+                    <div className="inner-box">
+                        <p className='contentpara'>New Feature</p>
+                    </div>
+                    <p className='contentpara'>Check out the team dashboard   <FontAwesomeIcon icon={faArrowRight} /></p>
+                </div>
+                <div className='analytics-container'>
+                    <h1>Beautiful analytics to grow smarter</h1>
+                    <p>Powerful, self-serve product and growth analytics to help you convert, engage, and retain more users. Trusted by over 4,000 startups.</p>
+                </div>
+                <div className='signup-container'>
+                    <button className='demo-button'> <FontAwesomeIcon icon={faPlayCircle} /> Demo</button>
+                    <button className='signup-button'>Sign Up</button>
+                </div>
+                <div className='stats-container'>
+                    <img
+                         className='stats-img'
+                        src={stats}
+                        alt="stats"
+                    />
+                </div>
+                <div className='stats-container2'>
+                    <img
+                         className='stats-img'
+                        src={stats2}
+                        alt="stats"
+                    />
+                </div>
+                {/* <Socialproofsection/>
+                <Featuressection/>
+                <Testimonialsection/>
+                <Advancedfeatures/>
+                <FaqSection/> */}
+            </div>
+        </div>
+    );
+};
+
+export default Header;
